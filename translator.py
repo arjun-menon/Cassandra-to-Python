b3e2c682c0e9b5d6a4e3617ef230662f1cc0b90b
Arjun Menon <contact@arjungmenon.com>
2011-08-23 19:14:37 -0400

    mm

--- translator.py
+++ translator.py
@@ -32,41 +32,45 @@ def iden(seq): # check if all elements in a sequence are identical
 
 ####################
 
-class Role(object):
-    def __init__(self, name):
-        self.name = name
-        # these members represent corresponding rules
-        self.canAcs = []
-        self.canDcs = []
-        self.isDacs = []
-    def __repr__(self):
-        return "\ncanActivate rules:\n" + repr(self.canAcs) + "\ncanDeactivate rules:\n" + repr(self.canDcs) + ", \nisDeactivated rules:\n" + repr(self.isDacs) + "\n"
+def build_roles():   
+    class Role(object):
+        def __init__(self, name):
+            self.name = name
+            # these members represent corresponding rules
+            self.canAcs = []
+            self.canDcs = []
+            self.isDacs = []
+        def __repr__(self):
+            return "\ncanActivate rules:\n" + repr(self.canAcs) + "\ncanDeactivate rules:\n" + repr(self.canDcs) + ", \nisDeactivated rules:\n" + repr(self.isDacs) + "\n"
+       
+    # Separating the role rules
+    the_three_predicates = ("canActivate", "canDeactivate", "isDeactivated")
+    canAc_rules = [rule for rule in rules if rule.concl.name == the_three_predicates[0]]
+    canDc_rules = [rule for rule in rules if rule.concl.name == the_three_predicates[1]]
+    isDac_rules = [rule for rule in rules if rule.concl.name == the_three_predicates[2]]
+    
+    # Get the names of unqiue roles
+    uniq_role_names = uniq([rule.concl.args[1].name for rule in canAc_rules])
+    
+    roles = dict((role_name, Role(role_name)) for role_name in uniq_role_names)
+    
+    for r in canAc_rules:
+        roles[r.concl.args[1].name].canAcs.append(r)
+    
+    for r in canDc_rules:
+        roles[r.concl.args[2].name].canDcs.append(r)
+    
+    for r in isDac_rules:
+        for h in r.hypos:
+            if type(h) == ehrparse.Atom and h.name == the_three_predicates[2]:
+                role_name = h.args[1].name
+        roles[role_name].isDacs.append(r)
+    
+    return roles
 
-# Separating rules into those associated with roles & not
-the_role_predicates = ("canActivate", "canDeactivate", "isDeactivated")
-role_rules = [rule for rule in rules if rule.concl.name in the_role_predicates]
-non_role_rules = [rule for rule in rules if rule.concl.name not in the_role_predicates]
 
-# Separating the role rules
-canAc_rules = [rule for rule in rules if rule.concl.name == the_role_predicates[0]]
-canDc_rules = [rule for rule in rules if rule.concl.name == the_role_predicates[1]]
-isDac_rules = [rule for rule in rules if rule.concl.name == the_role_predicates[2]]
+####################
 
-# Get the names of unqiue roles
-uniq_role_names = uniq([rule.concl.args[1].name for rule in canAc_rules])
 
-roles = dict([(role_name, Role(role_name)) for role_name in uniq_role_names])
-
-for r in canAc_rules:
-    roles[r.concl.args[1].name].canAcs.append(r)
-
-for r in canDc_rules:
-    roles[r.concl.args[2].name].canDcs.append(r)
-
-for r in isDac_rules:
-    for h in r.hypos:
-        if type(h) == ehrparse.Atom and h.name == 'isDeactivated':
-            role_name = h.args[1].name
-    roles[role_name].isDacs.append(r)
 
 repl()
